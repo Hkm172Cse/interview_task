07e05be3cfb8cdee35bb6cf8f165ffc928269f05
Hkm172Cse <azizulhakim000111@gmail.com>
2022-01-20 20:03:32 +0600

    database file included as folder name sql file here

---
+++ sql_file_here/ecomerce.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 20, 2022 at 03:02 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecomerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_user`
+--
+
+CREATE TABLE `table_user` (
+  `id` int(11) NOT NULL,
+  `user` varchar(150) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `table_user`
+--
+
+INSERT INTO `table_user` (`id`, `user`, `email`, `password`) VALUES
+(1, 'Hakim', 'abcd@gmail.com', 'sdfdsa'),
+(2, 'Hakim', 'abcd@gmail.com', 'sdfds'),
+(3, 'abc', 'abc@gmail.com', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `table_user`
+--
+ALTER TABLE `table_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `table_user`
+--
+ALTER TABLE `table_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
